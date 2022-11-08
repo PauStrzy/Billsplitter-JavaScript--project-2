13fb23acc5405f06e747144710ce306037e40e22
Paulina <paustrzy@gmail.com>
2022-11-08 23:01:46 +0100

    Created Billsplitter

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const price = document.querySelector('#price')
+const people = document.querySelector('#people')
+const tip = document.querySelector('#tip')
+const cost = document.querySelector('.cost')
+const costInfo = document.querySelector('.cost-info')
+const error = document.querySelector('.error')
+const btn = document.querySelector('.count')
+
+const checkInputs = () => {
+	if (price.value == '' || people.value == '' || tip.value == 0) {
+		error.textContent = 'Uzupełnij wszystkie pola!'
+		costInfo.style.display = 'none'
+	} else {
+		error.textContent = ''
+		countBill()
+	}
+}
+
+const countBill = () => {
+	const newPrice = parseFloat(price.value)
+	const newPeople = parseInt(people.value)
+	const newTip = parseFloat(tip.value)
+
+	const sum = (newPrice + newPrice * newTip) / newPeople
+	costInfo.style.display = 'block'
+
+	cost.textContent = sum.toFixed(2)
+}
+
+btn.addEventListener('click', checkInputs)
+
+//
